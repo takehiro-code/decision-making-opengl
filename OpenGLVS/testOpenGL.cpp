da8b2580795a6951f3f5229b623391e2f8215f13
Takehiro <tta46@sfu.ca>
2020-06-25 18:20:03 -0700

    Initial commit with test cpp file.

---
+++ OpenGLVS/testOpenGL.cpp
@@ -0,0 +1,183 @@
+﻿#include <iostream>
+// GLEW ------ Header
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW ------ Header
+// Please refer to more examples at: https://www.glfw.org/docs/latest/quick_guide.html
+//
+#include <GLFW/glfw3.h>
+//
+// Enable standard input and output via printf(), etc.
+// Put this include *after* the includes for glew and GLFE!
+#include <stdio.h>
+// #include <unistd.h> // added for the timer part (April 22)
+//
+// Window dimensions to be used in the call-back window size
+const GLuint WIDTH = 800, HEIGHT = 600;
+// Shaders ------ Extra steps to create the shader setup (not needed if not worry about shading)
+const GLchar* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+// The MAIN function, from here we start the application and run the game loop
+// Game loop here refers to as the continues execution of the program.
+int main()
+{
+	// Init GLFW -- initilizaing GLFW library
+	glfwInit();
+	// Set all the required options for GLFW
+	// The following is for creating a window and context
+	// https://www.glfw.org/docs/3.3/group__window.html
+	// This also basically makes sure the minimum version of OpenGL is satisfied
+	// and backward compatibilities.
+	// This would be the client API version which should be compatibale with the hints.
+	//
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+	int screenWidth, screenHeight;
+	glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+	if (nullptr == window)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+	// Making the OpenGL context current
+	//
+	glfwMakeContextCurrent(window);
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+	glewExperimental = GL_TRUE;
+	// Initialize GLEW to setup the OpenGL Function pointers
+	if (GLEW_OK != glewInit())
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return EXIT_FAILURE;
+	}
+	// Define the viewport dimensions
+	glViewport(0, 0, screenWidth, screenHeight);
+	// Build and compile our shader program
+	// Vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// Check for compile time errors
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// Fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// Check for compile time errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// Link shaders
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	// Set up vertex data (and buffer(s)) and attribute pointers
+	GLfloat vertices[] =
+	{
+	-0.5f, -0.5f, 0.0f, // Left
+	0.5f, -0.5f, 0.0f, // Right
+	0.0f, 0.5f, 0.0f, // Top
+	0.3f, 0.3f, 0.0f // If you want to draw a triangle, you need three points
+	};
+	// Another example for data set up for three points
+	// specify three vertices, three values (x,y,z) for each point (not used here)
+	GLfloat threeVerts[] =
+	{
+	0.0f, -0.5f, 0.0f, // First point
+	0.3f, -0.4f, 0.0f, // Second point
+	0.0f, 0.0f, 0.0f // Third point
+	};
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO); // changed to 1 to 2 if you want to use both arrays defined above
+	glGenBuffers(1, &VBO); // changed to 1 to 2 or to more if you are using more data sets
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+	// Game loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+		glfwPollEvents();
+		// Render
+		// Clear the colorbuffer
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Draw our first triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		// glDrawArrays(GL_TRIANGLES, 0, 3); // if you want to draw triangles, you need collection of three points
+		// Drawing points
+		// Render
+		// Clear the colorbuffer
+		// std::cin.get();
+		// glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		// glClear(GL_COLOR_BUFFER_BIT);
+		glPointSize(6); //Points are n pixels in diameter
+		glEnable(GL_POINT_SMOOTH);
+		glHint(GL_POINT_SMOOTH_HINT, GL_NICEST);
+		glDrawArrays(GL_POINTS, 0, 6);
+		//
+		// Drawing Lines
+		//
+		// glVertexAttrib3f(vertColor_loc, 0.5f, 1.0f, .2f);
+		glLineWidth(3); // Lines are m pixels wide
+		glEnable(GL_LINE_SMOOTH);
+		glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+		glDrawArrays(GL_LINES, 0, 6);
+		//
+		glBindVertexArray(0);
+		// Swap the screen buffers. This is a double buffering (front buffer and back buffer)
+		// 60-75 times per second.
+		//
+		glfwSwapBuffers(window);
+	}
+	// Properly de-allocate all resources once they've outlived their purpose
+	glDeleteVertexArrays(1, &VAO);
+
+	glDeleteBuffers(1, &VBO);
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glfwTerminate();
+	return EXIT_SUCCESS;
+}
